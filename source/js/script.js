d0b0f418988f052e498547570cfa8c66f5189ec0
garold664 <garold664@gmail.com>
2019-10-22 11:56:12 +0300

    added saving user-name in local-storage

--- source/js/script.js
+++ source/js/script.js
@@ -7,7 +7,7 @@ var servicesSlides = document.querySelector('.services__slides');
 var activeSlideClass = 'services__slide--active';
 
 
-servicesNav.addEventListener('click', function(evt) {
+servicesNav.addEventListener('click', function (evt) {
   var target = evt.target;
   var linkClass = 'services__nav-link';
   var linkActiveClass = 'services__nav-link--active';
@@ -34,8 +34,20 @@ servicesNav.addEventListener('click', function(evt) {
 
 var openPopupBtn = document.querySelector('#open-popup');
 var popup = document.querySelector('#popup');
+var form = popup.querySelector('.popup__form');
+var userName = form.querySelector('[name=user-name');
+var userEmail = form.querySelector('[name=user-email');
 var closePopupBtn = popup.querySelector('.popup__close-btn');
-var firstInput = popup.querySelector('input:first-of-type');
+
+
+var isStorageSupport = true;
+var storage = '';
+
+try {
+  storage = localStorage.getItem('user-name');
+} catch (err) {
+  isStorageSupport = false;
+}
 
 var closePopupHandler = function() {
   if (popup.classList.contains('popup--show')) {
@@ -43,24 +55,42 @@ var closePopupHandler = function() {
   }
 };
 
-openPopupBtn.addEventListener('click', function(evt) {
+openPopupBtn.addEventListener('click', function (evt) {
   evt.preventDefault();
   popup.classList.add('popup--show');
-  firstInput.focus();
+
+  if (storage) {
+    userName.value = storage;
+    userEmail.focus();
+  } else {
+    userName.focus();
+  }
 });
 
-popup.addEventListener('click', function(evt) {
-  evt.preventDefault();
+popup.addEventListener('click', function (evt) {
   if ((evt.target.classList.contains('popup__close-btn')) ||
      (evt.target.classList.contains('popup__overlay')))  {
+
+    evt.preventDefault();
     closePopupHandler();
   }
 
 });
 
-document.addEventListener('keydown', function(evt) {
-  evt.preventDefault();
+window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27)  {
+    evt.preventDefault();
     closePopupHandler();
   }
 });
+
+form.addEventListener('submit', function (evt) {
+  console.log('submit');
+  if (!userName.value || !userEmail.value) {
+    evt.preventDefault();
+    console.log('Нужно ввести имя и email');
+  } else if (isStorageSupport) {
+    localStorage.setItem('user-name', userName.value)
+  }
+});
+
